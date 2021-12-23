d43cd50377f0055891b6b3493acb55e0f2f09da0
ddfilipov <denisfilipov12+3@gmail.com>
2021-12-23 22:45:37 +0100

    pressiong the button will roll a random number between 1 and 6

--- src/components/organisms/DiceSelector.tsx
+++ src/components/organisms/DiceSelector.tsx
@@ -2,10 +2,23 @@ import { FC } from "react";
 import { Die } from "./Die";
 
 export const DiceSelector: FC = () => {
+    const sixSidedDie = [1, 2, 3, 4, 5, 6];
+    const getRandomNumber = (die: number[]) => {
+        const dieNumber = Math.floor(Math.random() * die.length);
+        console.log(die[dieNumber]);
+        return die[dieNumber];
+    };
     return (
         <div>
             <Die />
-            <button type="button">Roll</button>
+            <button
+                type="button"
+                onClick={() => {
+                    getRandomNumber(sixSidedDie);
+                }}
+            >
+                Roll
+            </button>
         </div>
     );
 };
--- src/components/organisms/Die.tsx
+++ src/components/organisms/Die.tsx
@@ -1,6 +1,7 @@
 import { FC } from "react";
 
 export const Die: FC = () => {
+
     return (
         <div>
             <p>image of a 6-sided die goes here </p>
