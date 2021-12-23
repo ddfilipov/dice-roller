424e138174f52c39bcecc6a0764941c887e5bc77
ddfilipov <denisfilipov12+3@gmail.com>
2021-12-23 23:54:30 +0100

    restored some files that made possible to use .svgs and now rolling the die will change the state and show info on the Die component

--- src/components/organisms/DiceSelector.tsx
+++ src/components/organisms/DiceSelector.tsx
@@ -1,16 +1,30 @@
-import { FC } from "react";
-import { Die } from "./Die";
+import { FC, useState } from "react";
+import { Die, DieProps } from "./Die";
+import d6s6 from "./d6.svg";
+
+export interface RollType {
+    side: number;
+    icon: string;
+}
 
 export const DiceSelector: FC = () => {
-    const sixSidedDie = [1, 2, 3, 4, 5, 6];
-    const getRandomNumber = (die: number[]) => {
-        const dieNumber = Math.floor(Math.random() * die.length);
-        console.log(die[dieNumber]);
-        return die[dieNumber];
+    const [die, setDie] = useState<RollType>({ side: 1, icon: "uno" });
+
+    const sixSidedDie = {
+        side: [1, 2, 3, 4, 5, 6],
+        icon: ["uno", "dos", "tres", "cuatro", "cinco", "seis"],
     };
+    // const sixSidedDie = [1, 2, 3, 4, 5, 6];
+    const getRandomNumber = (die: DieProps) => {
+        const dieNumber = Math.floor(Math.random() * die.side.length);
+        const dieSide = die.side[dieNumber];
+        const dieIcon = die.icon[dieNumber];
+        setDie({ side: dieSide, icon: dieIcon });
+    };
+
     return (
         <div>
-            <Die />
+            <Die roll={die} />
             <button
                 type="button"
                 onClick={() => {
--- src/components/organisms/Die.tsx
+++ src/components/organisms/Die.tsx
@@ -1,10 +1,24 @@
 import { FC } from "react";
+import { RollType } from "./DiceSelector";
 
-export const Die: FC = () => {
+export interface DieProps {
+    side: number[];
+    icon: string[];
+}
 
+interface DieList {
+    dado: DieProps;
+}
+
+interface RollTypeCombo {
+    roll: RollType;
+}
+
+export const Die: FC<RollTypeCombo> = ({ roll }) => {
     return (
         <div>
-            <p>image of a 6-sided die goes here </p>
+            <p>Number: {roll.side}</p>
+            <p>Icon:{roll.icon} </p>
         </div>
     );
 };
