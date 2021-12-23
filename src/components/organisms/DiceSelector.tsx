b8637c6f92ee4f0a86c7abda48c56c181163f892
ddfilipov <denisfilipov12+3@gmail.com>
2021-12-23 22:33:59 +0100

    added a new component

--- src/components/organisms/DiceSelector.tsx
+++ src/components/organisms/DiceSelector.tsx
@@ -1,8 +1,10 @@
 import { FC } from "react";
+import { Die } from "./Die";
 
 export const DiceSelector: FC = () => {
     return (
         <div>
+            <Die />
             <button type="button">Roll</button>
         </div>
     );
---
+++ src/components/organisms/Die.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+export const Die: FC = () => {
+    return (
+        <div>
+            <p>image of a 6-sided die goes here </p>
+        </div>
+    );
+};
